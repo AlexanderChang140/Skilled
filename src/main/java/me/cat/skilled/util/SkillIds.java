637714f48e6fad8c99d29d947082bac3e5114e21
Alex <alexanderchang140@gmail.com>
2024-10-05 06:14:33 -0700

    feat: update skill ids

--- src/main/java/me/cat/skilled/util/SkillIds.java
+++ src/main/java/me/cat/skilled/util/SkillIds.java
@@ -1,9 +1,17 @@
 package me.cat.skilled.util;
 
 public class SkillIds {
-    public final static String BARRIER_SKILL = "barrier";
-    public final static String POWER_SHOT_SKILL = "power_shot";
-    public final static String ENDER_SHOT_SKILL = "ender_shot";
-    public final static String FLEETFOOTED_SKILL = "fleetfooted";
-    public final static String LIFESTEAL_SKILL = "lifesteal";
+    public final static String BARRIER = "barrier";
+
+    // Ranger
+    public final static String POWER_SHOT = "power_shot";
+    public final static String ENDER_SHOT = "ender_shot";
+    public final static String FLEETFOOTED = "fleetfooted";
+
+    // Warrior
+    public final static String LIFESTEAL = "lifesteal";
+    public final static String FRENZY = "frenzy";
+    public final static String LAST_STAND = "last_stand";
+    public final static String WHIRLWIND = "whirlwind";
+    public final static String PARRY = "parry";
 }
