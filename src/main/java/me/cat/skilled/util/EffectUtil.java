fa23d71fe5274123bc83e8b19f85ab5c0b492d11
Alex <alexanderchang140@gmail.com>
2024-10-05 06:17:27 -0700

    fix: change ambient and visible to false by default feat: add overload that includes ambient and visible parameters

--- src/main/java/me/cat/skilled/util/EffectUtil.java
+++ src/main/java/me/cat/skilled/util/EffectUtil.java
@@ -3,8 +3,21 @@ package me.cat.skilled.util;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 
 public class EffectUtil {
+
+    public static void setEffectDuration(LivingEntity livingEntity, MobEffect effect, int duration) {
+        if (!livingEntity.hasEffect(effect)) {
+            return;
+        }
+
+        MobEffectInstance mobEffectInstance = livingEntity.getEffect(effect);
+        int amplifier = mobEffectInstance.getAmplifier();
+
+        livingEntity.removeEffect(effect);
+        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier, false, false));
+    }
 
     public static void incrementEffect(LivingEntity livingEntity, MobEffect effect) {
         if (!livingEntity.hasEffect(effect)) {
@@ -16,11 +29,11 @@ public class EffectUtil {
         int amplifier = mobEffectInstance.getAmplifier();
 
         livingEntity.removeEffect(effect);
-        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier + 1));
+        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier + 1, false, false));
     }
 
     public static void incrementEffect(LivingEntity livingEntity, MobEffect effect, int amount) {
-        if (!livingEntity.hasEffect(effect)) {
+        if (!livingEntity.hasEffect(effect) || amount <= 0) {
             return;
         }
 
@@ -29,7 +42,20 @@ public class EffectUtil {
         int amplifier = mobEffectInstance.getAmplifier();
 
         livingEntity.removeEffect(effect);
-        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier + amount));
+        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier + amount, false, false));
+    }
+
+    public static void incrementEffect(LivingEntity livingEntity, MobEffect effect, int amount, boolean ambient, boolean visible) {
+        if (!livingEntity.hasEffect(effect) || amount <= 0) {
+            return;
+        }
+
+        MobEffectInstance mobEffectInstance = livingEntity.getEffect(effect);
+        int duration = mobEffectInstance.getDuration();
+        int amplifier = mobEffectInstance.getAmplifier();
+
+        livingEntity.removeEffect(effect);
+        livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier + amount, ambient, visible));
     }
 
     public static void decrementEffect(LivingEntity livingEntity, MobEffect effect) {
@@ -43,7 +69,7 @@ public class EffectUtil {
 
         livingEntity.removeEffect(effect);
         if (amplifier > 0) {
-            livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier - 1));
+            livingEntity.addEffect(new MobEffectInstance(effect, duration, amplifier - 1, false, false));
         }
     }
 
@@ -58,7 +84,22 @@ public class EffectUtil {
 
         livingEntity.removeEffect(effect);
         if (amplifier > 0) {
-            livingEntity.addEffect(new MobEffectInstance(effect, duration, Math.max(amplifier - amount, 0)));
+            livingEntity.addEffect(new MobEffectInstance(effect, duration, Math.max(amplifier - amount, 0), false, false));
+        }
+    }
+
+    public static void decrementEffect(LivingEntity livingEntity, MobEffect effect, int amount, boolean ambient, boolean visible) {
+        if (!livingEntity.hasEffect(effect)) {
+            return;
+        }
+
+        MobEffectInstance mobEffectInstance = livingEntity.getEffect(effect);
+        int duration = mobEffectInstance.getDuration();
+        int amplifier = mobEffectInstance.getAmplifier();
+
+        livingEntity.removeEffect(effect);
+        if (amplifier > 0) {
+            livingEntity.addEffect(new MobEffectInstance(effect, duration, Math.max(amplifier - amount, 0), ambient, visible));
         }
     }
 }
