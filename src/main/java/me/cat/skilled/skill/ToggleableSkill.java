30832a5635e27e29d29cfedd7e787d8aef3d7c22
Alex <alexanderchang140@gmail.com>
2025-03-16 16:10:01 -0700

    feat: add toggleable active skill

---
+++ src/main/java/me/cat/skilled/skill/ToggleableSkill.java
@@ -0,0 +1,69 @@
+package me.cat.skilled.skill;
+
+import me.cat.skilled.util.TickTimer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.awt.*;
+
+public class ToggleableSkill extends ActiveSkill {
+    private final TickTimer toggleTimer = new TickTimer(20);
+    private boolean canToggle = true;
+    protected boolean isToggled = false;
+    protected Color toggledColor = Color.YELLOW;
+
+    public ToggleableSkill(int ticksPerAction) {
+        super(ticksPerAction);
+    }
+
+    @Override
+    protected boolean onActivateSkill(ServerPlayer serverPlayer) {
+        if (canToggle) {
+            isToggled = !isToggled;
+            canToggle = false;
+        }
+        return false;
+    }
+
+    public void tickToggleTimer() {
+        if (!canToggle && toggleTimer.doTick()) {
+            canToggle = true;
+        }
+    }
+
+    @Override
+    public CompoundTag saveNbt() {
+        CompoundTag tag = super.saveNbt();
+        tag.putInt("toggle_timer", toggleTimer.getTickCounter());
+        tag.putBoolean("can_toggle", canToggle);
+        tag.putBoolean("is_toggled", isToggled);
+        return tag;
+    }
+
+    @Override
+    public void loadNbt(CompoundTag tag) {
+        super.loadNbt(tag);
+        toggleTimer.setTickCounter(tag.getInt("toggle_timer"));
+        canToggle = tag.getBoolean("can_toggle");
+        isToggled = tag.getBoolean("is_toggled");
+    }
+
+    protected void setToggled(boolean toggled) {
+        isToggled = toggled;
+    }
+
+    @Override
+    public int getCurrTick() {
+        return isToggled ? -1 : super.getCurrTick();
+    }
+
+    @Override
+    public boolean showCooldown() {
+        return isToggled || super.showCooldown();
+    }
+
+    @Override
+    public Color getCooldownColorValues() {
+        return isToggled ? toggledColor : super.getCooldownColorValues();
+    }
+}
