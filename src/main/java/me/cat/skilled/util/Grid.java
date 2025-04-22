8e41bd5ef8abf09fcd76ec4a3ca2038b04d311a5
Alex <alexanderchang140@gmail.com>
2025-04-22 01:50:53 -0700

    refactor: simplify grid

--- src/main/java/me/cat/skilled/util/Grid.java
+++ src/main/java/me/cat/skilled/util/Grid.java
@@ -1,25 +1,13 @@
 package me.cat.skilled.util;
 
 public class Grid {
-    private static final int DEFAULT_OFFSET = 8;
-    private static final int TIER_OFFSET = 30;
-    private static final int LINE_OFFSET = 60;
+    private final int offset;
 
-    public static int getPos(int index) {
-        return index * DEFAULT_OFFSET;
+    public Grid(int offset) {
+        this.offset = offset;
     }
 
-    public static int getPos(int index, int offset) {
+    public int getPos(int index) {
         return offset * index;
-    }
-
-    public static int tierY(int tier) {
-        int index = tier - 3;
-        return -index * TIER_OFFSET - TIER_OFFSET / 2;
-    }
-
-    public static int lineX(int line) {
-        int index = line - 2;
-        return index * LINE_OFFSET;
     }
 }
