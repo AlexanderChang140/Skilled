388f8f0eb2090433f67726bf9a06157dd720edad
Alex <alexanderchang140@gmail.com>
2025-04-22 01:26:43 -0700

    style: enforce final before static

--- src/main/java/me/cat/skilled/util/Utilities.java
+++ src/main/java/me/cat/skilled/util/Utilities.java
@@ -4,7 +4,7 @@ import me.cat.skilled.Skilled;
 import net.minecraft.resources.ResourceLocation;
 
 public class Utilities {
-    private final static String DEFAULT_ICON_PATH = "textures/skill/";
+    private static final String DEFAULT_ICON_PATH = "textures/skill/";
 
     public static ResourceLocation getDefaultSkillIconPath(String fileName) {
         return new ResourceLocation(Skilled.MODID, DEFAULT_ICON_PATH + fileName);
