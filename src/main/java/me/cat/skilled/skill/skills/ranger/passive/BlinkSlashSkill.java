43d9679c32e319ded8c581777370fa27607c782c
Alex <alexanderchang140@gmail.com>
2025-04-23 00:02:52 -0700

    feat: add error message for failed subscription

--- src/main/java/me/cat/skilled/skill/skills/ranger/passive/BlinkSlashSkill.java
+++ src/main/java/me/cat/skilled/skill/skills/ranger/passive/BlinkSlashSkill.java
@@ -3,6 +3,7 @@ package me.cat.skilled.skill.skills.ranger.passive;
 import me.cat.skilled.registry.SkillRegistry;
 import me.cat.skilled.skill.Skill;
 import me.cat.skilled.skill.skills.ranger.active.EnderShotSkill;
+import me.cat.skilled.util.Utilities;
 import me.cat.skilled.util.event.Subscriber;
 
 public class BlinkSlashSkill extends Skill implements Subscriber<Void, Void> {
@@ -10,6 +11,9 @@ public class BlinkSlashSkill extends Skill implements Subscriber<Void, Void> {
     public void init() {
         if (skillMap.get(SkillRegistry.ENDER_SHOT.getSkillId()) instanceof EnderShotSkill enderShotSkill) {
             enderShotSkill.getTeleportEvent().subscribe(this);
+        }
+        else {
+            Utilities.failedSubscription(skillData.getSkillId(), SkillRegistry.ENDER_SHOT.getSkillId());
         }
     }
 
